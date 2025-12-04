648d1c9deed60e1ed2b5ce3ee2738fcdbc59f80d
bruce1408 <summer56567@163.com>
2025-12-04 17:47:03 +0800

    Correct expected result for adding zero in test_main.py

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ class TestMain(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 1)
+        self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
